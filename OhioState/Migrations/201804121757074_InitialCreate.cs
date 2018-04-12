5357ec60292f356cb998bddf65225997c979be3e
Zahid Iqbal <zahid.iqbal@amtrustgroup.com>
2018-04-12 14:41:04 -0400

    enabled Code First Migrations, skipped deploy to Azure.

---
+++ OhioState/Migrations/201804121757074_InitialCreate.cs
@@ -0,0 +1,62 @@
+namespace OhioState.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialCreate : DbMigration
+    {
+        /// <summary>
+        /// Migrations calls the Up method to implement the data model changes for a migration. When you enter a command to roll back the update, Migrations calls the Down method.
+        /// </summary>
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Course",
+                c => new
+                    {
+                        CourseID = c.Int(nullable: false),
+                        Title = c.String(),
+                        Credits = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.CourseID);
+            
+            CreateTable(
+                "dbo.Enrollment",
+                c => new
+                    {
+                        EnrollmentID = c.Int(nullable: false, identity: true),
+                        CourseID = c.Int(nullable: false),
+                        StudentID = c.Int(nullable: false),
+                        Grade = c.Int(),
+                    })
+                .PrimaryKey(t => t.EnrollmentID)
+                .ForeignKey("dbo.Course", t => t.CourseID, cascadeDelete: true)
+                .ForeignKey("dbo.Student", t => t.StudentID, cascadeDelete: true)
+                .Index(t => t.CourseID)
+                .Index(t => t.StudentID);
+            
+            CreateTable(
+                "dbo.Student",
+                c => new
+                    {
+                        ID = c.Int(nullable: false, identity: true),
+                        LastName = c.String(),
+                        FirstMidName = c.String(),
+                        EnrollmentDate = c.DateTime(nullable: false),
+                    })
+                .PrimaryKey(t => t.ID);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Enrollment", "StudentID", "dbo.Student");
+            DropForeignKey("dbo.Enrollment", "CourseID", "dbo.Course");
+            DropIndex("dbo.Enrollment", new[] { "StudentID" });
+            DropIndex("dbo.Enrollment", new[] { "CourseID" });
+            DropTable("dbo.Student");
+            DropTable("dbo.Enrollment");
+            DropTable("dbo.Course");
+        }
+    }
+}
